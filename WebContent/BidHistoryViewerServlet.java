c97afa5e95ef36887a5b3f19cbb9e2c0b46421fb
fadu94 <79604514+fadu94@users.noreply.github.com>
2021-04-24 20:26:36 -0400

    Add files via upload

---
+++ WebContent/BidHistoryViewerServlet.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import persistence.repository.BidRepository;
+
+/** Servlet to view history of bids in any auction **/
+
+@WebServlet("/HistoryViewer")
+public class BidHistoryViewerServlet extends HttpServlet {
+	
+	private final BidRepository bidRepo = new BidRepository();
+	
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+	{
+		String loginID = req.getParameter("loginId");
+		List<Bid> history = bidRepo.findAllBids();
+	
+		for (Bid b : history)
+		{
+			if (b.getUser().getloginId() == loginID)
+				System.out.println(b.getBidId() + ", " + b.getItem().getItemId() + ", " + b.getItem().getBrand());
+		}
+		
+	}
+}
